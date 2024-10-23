49287fe7b8afaa048f0905ba9f44471c9c2e3aee
Arturo Suárez <manuel.suarez@cimat.mx>
2024-10-22 23:56:22 -0600

    Filter by date according to sentinel-1 and sentinel-2 operating start dates

--- read-dataset.py
+++ read-dataset.py
@@ -9,6 +9,8 @@ data_path = os.path.join(base_path, "data", "cimat", "incidents")
 
 # Open CSV file, lat and lon are the coordinates
 incidents_df = pd.read_csv(os.path.join(data_path, "incidents.csv"))
+# Convert the datetime
+incidents_df["open_date"] = pd.to_datetime(incidents_df["open_date"], format="%Y-%m-%d")
 
 # Create a GeoDataFrame from DataFrame
 geometry = [Point(xy) for xy in zip(incidents_df["lon"], incidents_df["lat"])]
@@ -26,8 +28,24 @@ bbox = Polygon(bbox_coords)
 # Filter rows within bounding box
 incidents_gm_gdf = incidents_gdf[incidents_gdf.within(bbox)]
 
+# Define Sentinel-1, Sentinel-2 operating dates to filter incidents by date range
+incidents_gm_sen1_gdf = incidents_gm_gdf.loc[
+    (incidents_gm_gdf["open_date"] > "2014-04-03")
+]
+incidents_gm_sen2_gdf = incidents_gm_gdf.loc[
+    (incidents_gm_gdf["open_date"] > "2015-06-23")
+]
+
 # Save filtered data
 print(incidents_gm_gdf)
+print(incidents_gm_sen1_gdf)
+print(incidents_gm_sen2_gdf)
 
 
 incidents_gm_gdf.to_csv(os.path.join(data_path, "gm_incidents.csv"), index=False)
+incidents_gm_sen1_gdf.to_csv(
+    os.path.join(data_path, "gm_sen1_incidents.csv"), index=False
+)
+incidents_gm_sen2_gdf.to_csv(
+    os.path.join(data_path, "gm_sen2_incidents.csv"), index=False
+)
