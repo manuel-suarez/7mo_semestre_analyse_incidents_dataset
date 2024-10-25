2fcd88e40f485c50d331e467c4c63045550e3337
Arturo Suárez <manuel.suarez@cimat.mx>
2024-10-25 16:39:48 -0600

    Make products list per satellite, date and coordinates

--- productlist-dataset.py
+++ productlist-dataset.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date, timedelta
+from tqdm import tqdm
 import requests
 import pandas as pd
 import geopandas as gpd
@@ -41,46 +42,93 @@ def get_keycloak(username: str, password: str) -> str:
     return r.json()["access_token"]
 
 
-json_ = requests.get(
-    f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=Collection/Name eq '{data_collection}' and OData.CSC.Intersects(area=geography'SRID=4326;{ft}') and ContentDate/Start gt {yesterday_string}T00:00:00.000Z and ContentDate/Start lt {today_string}T00:00:00.000Z&$count=True&$top=1000"
-).json()
-p = pd.DataFrame.from_dict(json_["value"])
-if p.shape[0] > 0:
-    p["geometry"] = p["GeoFootprint"].apply(shape)
-    # Convert pandas dataframe to Geopandas dataframe by setting up geometry
-    productDF = gpd.GeoDataFrame(p).set_geometry("geometry")
-    # Remove L1C dataset if not needed
-    productDF = productDF[~productDF["Name"].str.contains("L1C")]
-    print(f" total L2A tiles found {len(productDF)}")
-    productDF["identifier"] = productDF["Name"].str.split(".").str[0]
-    allfeat = len(productDF)
+def get_products(data_collection, geometry, date_string):
+    json_ = requests.get(
+        f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=Collection/Name eq '{data_collection}' and OData.CSC.Intersects(area=geography'SRID=4326;{geometry}') and ContentDate/Start gt {date_string}T00:00:00.000Z and ContentDate/Start lt {date_string}T23:59:59.000Z&$count=True&$top=1000"
+    ).json()
+    # print(json_)
+    p = pd.DataFrame.from_dict(json_["value"])
+    if p.shape[0] > 0:
+        p["geometry"] = p["GeoFootprint"].apply(shape)
+        # Convert pandas dataframe to Geopandas dataframe by setting up geometry
+        productDF = gpd.GeoDataFrame(p).set_geometry("geometry")
+        # Remove L1C dataset if not needed
+        productDF = productDF[~productDF["Name"].str.contains("L1C")]
+        print(f" total L2A tiles found {len(productDF)}")
+        productDF["identifier"] = productDF["Name"].str.split(".").str[0]
+        allfeat = len(productDF)
 
-    if allfeat == 0:
-        print(f"No tiles found for {today_string}")
+        if allfeat == 0:
+            return "No tiles found for {date_string}"
+        else:
+            response = [feat["properties"]["Name"] for feat in productDF.iterfeatures()]
+            return response
     else:
-        for index, feat in enumerate(productDF.iterfeatures()):
-            try:
-                # Create requests session
-                session = requests.Session()
-                # Get access token based on username and password
-                keycloak_token = get_keycloak(copernicus_user, copernicus_password)
+        return ""
+        # else:
+        #    for index, feat in enumerate(productDF.iterfeatures()):
+        #        try:
+        #            # Create requests session
+        #            session = requests.Session()
+        #            # Get access token based on username and password
+        #            keycloak_token = get_keycloak(copernicus_user, copernicus_password)
 
-                session.headers.update({"Authorization": f"Bearer {keycloak_token}"})
-                url = f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products({feat['properties']['Id']})/$value"
-                response = session.get(url, allow_redirects=False)
-                while response.status_code in (301, 302, 303, 307):
-                    url = response.headers["Location"]
-                    response = session.get(url, allow_redirects=False)
-                print(feat["properties"]["Id"])
-                file = session.get(url, verify=False, allow_redirects=True)
+        #            session.headers.update(
+        #                {"Authorization": f"Bearer {keycloak_token}"}
+        #            )
+        #            url = f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products({feat['properties']['Id']})/$value"
+        #            response = session.get(url, allow_redirects=False)
+        #            while response.status_code in (301, 302, 303, 307):
+        #                url = response.headers["Location"]
+        #                response = session.get(url, allow_redirects=False)
+        #            print(feat["properties"]["Id"])
+        #            file = session.get(url, verify=False, allow_redirects=True)
 
-                with open(
-                    f"location/to/save/{feat['properties']['identifier']}.zip",  # location to save zip from copernicus
-                    "wb",
-                ) as p:
-                    print(feat["properties"]["Name"])
-                    p.write(file.content)
-            except:
-                print("problem with server")
-else:
-    print("no data found")
+        #            with open(
+        #                f"location/to/save/{feat['properties']['identifier']}.zip",  # location to save zip from copernicus
+        #                "wb",
+        #            ) as p:
+        #                print(feat["properties"]["Name"])
+        #                p.write(file.content)
+        #        except:
+        #            print("problem with server")
+        # else:
+        #    print("no data found")
+
+
+def open_and_search_products(inputfile, outputfile, satellite):
+    incidents_ds = gpd.read_file(inputfile)
+    print(incidents_ds)
+    print(type(incidents_ds["geometry"]))
+
+    data = {
+        "date": [],
+        "products": [],
+        "geometry": [],
+    }
+
+    for index, row in tqdm(incidents_ds.iterrows()):
+        coordinates = row.geometry
+        date = row.open_date
+        prods = get_products(satellite, coordinates, date)
+        # print(index, date, coordinates, prods)
+        data["date"].append(date)
+        data["products"].append(prods)
+        data["geometry"].append(coordinates)
+    # save result
+    df = pd.DataFrame(data)
+    df.to_csv(outputfile, index=False)
+
+
+# We need to open CSV dataset and get date and coordinates to search a Sentinel 1/2 product
+if __name__ == "__main__":
+    # Define paths
+
+    base_path = os.path.expanduser("~")
+    data_path = os.path.join(base_path, "data", "cimat", "incidents")
+    input_file = os.path.join(data_path, "gm_sen2_incidents.csv")
+    output_file = os.path.join(data_path, "sentinel2_products.csv")
+
+    # Open CSV file with geopandas to get access to dates and coordinates
+
+    open_and_search_products(input_file, output_file, "SENTINEL-2")
